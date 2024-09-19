c948744efe1c0a0148ae01b1eff1fb71f1b310f0
EDIVINA1 <2023akit066gf@kab.ac.ug>
2024-09-19 18:40:30 +0300

    Add files via upload

---
+++ index.php
@@ -0,0 +1,19 @@
+
+<?php
+
+$name="NIWABIINE EDIVINA";
+$DOB="07/07/2003";
+$Address="IBANDA";
+$CYear=2024;
+$BYear=2003;
+$Age=$CYear-$BYear;
+echo"Name: ".$name. "<br>";
+echo"Date Of Birth : ".$DOB."<br>";
+echo"Home Address: ".$Address."<br>";
+echo"Welcome home, ".$name."<br>";
+echo"You are now , ".$Age."Years old";
+/* DOB stands for date of birth
+CYEAR stands for current year
+BYEAR stands for year of birth*/
+?> 
+
